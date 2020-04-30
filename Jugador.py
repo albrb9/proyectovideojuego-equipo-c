87e22205ba83c78fc567c8d9c8685a2644e21cfd
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2020-04-30 19:56:48 +0200

    Disparo jugador
    He arreglado las rutas con el os.path.sep y añadido el método disparar. De momento está asignado a la Q.

--- Jugador.py
+++ Jugador.py
@@ -1,4 +1,5 @@
 import arcade
+import os
 
 # Constantes para mirar a que lado mira
 RIGHT_FACING = 0
@@ -11,21 +12,10 @@ UPDATES_PER_FRAME = 10
 NUM_TEXTURAS_ANDAR = 2
 
 
-def load_texture_pair(filename):
-    """
-    Load a texture pair, with the second being a mirror image.
-    (El espejo no hace nada para sprites de mirar arriba o abajo)
-    """
-    return [
-        arcade.load_texture(filename),
-        arcade.load_texture(filename, mirrored=True)
-    ]
-
-
 def load_texture_4dir(filename_lados, filename_up, filename_down):
     """
-    Load a texture pair, with the second being a mirror image.
-    (El espejo no hace nada para sprites de mirar arriba o abajo)
+    Carga texturas en las 4 direcciones, siendo la seguna un versión espejo del
+    primer argumento.
     """
     return [
         arcade.load_texture(filename_lados),
@@ -37,6 +27,7 @@ def load_texture_4dir(filename_lados, filename_up, filename_down):
 
 class Jugador(arcade.Sprite):
     def __init__(self):
+        """Constructor del sprite del jugador"""
         super().__init__()
 
         # Dirección a la que mira por defecto
@@ -46,19 +37,49 @@ class Jugador(arcade.Sprite):
         self.cur_texture = 0
 
         # ---Cargar texturas---
-        self.textura_quieto = load_texture_4dir("sprites_master/PERSONAJE10.png", "sprites_master/PERSONAJE7.png",
-                                                "sprites_master/PERSONAJE4.png")
+        self.textura_quieto = load_texture_4dir("sprites_master" + os.path.sep + "PERSONAJE10.png",
+                                                "sprites_master" + os.path.sep + "PERSONAJE7.png",
+                                                "sprites_master" + os.path.sep + "PERSONAJE4.png")
         self.textura_andando = []
         for i in (11, 12):
             # Cargamos texturas 11 y 12 (derecha moviendo los pies)
             # 8 y 9 (arriba moviendo los pies)
             # 5 y 6 (abajo moviendo los pies)
-            self.textura_andando.append(load_texture_4dir("sprites_master/PERSONAJE{}.png".format(i),
-                                                          "sprites_master/PERSONAJE{}.png".format(i - 3),
-                                                          "sprites_master/PERSONAJE{}.png".format(i - 6)))
+            self.textura_andando.append(load_texture_4dir("sprites_master" + os.path.sep + "PERSONAJE{}.png".format(i),
+                                                          "sprites_master" + os.path.sep + "PERSONAJE{}.png".format(
+                                                              i - 3),
+                                                          "sprites_master" + os.path.sep + "PERSONAJE{}.png".format(
+                                                              i - 6)))
+
+    def disparar(self, jugador, velocidad_disparo):
+        """ArcadeSprite, int ---> ArcadeSprite
+        Crea y calcula la trayectoria de la bala y retorna esta"""
+        # Crear la bala
+        bala = arcade.Sprite("sprites_master"+os.path.sep+"BALA.png")
+        # Calcular su trayectoria y su posicion de spawn
+        if self.character_face_direction == RIGHT_FACING:
+            bala.left = jugador.right
+            bala.center_y = jugador.center_y
+            bala.change_x = velocidad_disparo
+        elif self.character_face_direction == LEFT_FACING:
+            bala.right = jugador.left
+            bala.center_y = jugador.center_y
+            bala.change_x = -velocidad_disparo
+        elif self.character_face_direction == UP_FACING:
+            bala.bottom = jugador.top
+            bala.center_x = jugador.center_x
+            bala.change_y = velocidad_disparo
+        elif self.character_face_direction == DOWN_FACING:
+            bala.top = jugador.bottom
+            bala.center_x = jugador.center_x
+            bala.change_y = -velocidad_disparo
+        return bala
+
+
+
 
     def update_animation(self, delta_time: float = 1 / 60):
-
+        """Utilizado para actualizar la animación del jugador"""
         # Vemos adonde tenemos que mirar
         if self.change_x < 0 and (self.character_face_direction == RIGHT_FACING or UP_FACING or DOWN_FACING):
             self.character_face_direction = LEFT_FACING
--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import arcade
 import Jugador
+import os
+import math
+
 # --- Constantes ---
 
 # SPRITE_SCALING_BOX = 0.5
@@ -46,7 +49,7 @@ def setup_room_1():
     # Fila de abajo y arriba de pared
     for y in (0, SCREEN_HEIGHT - ANCHO_MURO):
         for x in range(0, SCREEN_WIDTH, LARGO_MURO):
-            muro = arcade.Sprite("sprites_master\\Muro2.png")
+            muro = arcade.Sprite("sprites_master" + os.path.sep + "Muro2.png")
             muro.left = x
             muro.bottom = y
             room.wall_list.append(muro)
@@ -54,7 +57,7 @@ def setup_room_1():
     # Fila de la izquierda y la derecha
     for x in (0, SCREEN_WIDTH - ANCHO_MURO):
         for y in range(0, SCREEN_HEIGHT - ANCHO_MURO, LARGO_MURO):
-            muro = arcade.Sprite("sprites_master\\Muro.png")
+            muro = arcade.Sprite("sprites_master" + os.path.sep + "Muro.png")
             if not (300 <= y <= 400) or x == 0:  # Agujero en la parte izquierda para pasar a la siguiente habitacion
                 muro.left = x
                 muro.bottom = y
@@ -64,11 +67,10 @@ def setup_room_1():
     # Con pequeños trozos de suelo
     # for x in range(ANCHO_MURO, SCREEN_WIDTH - TAM_SUELO, TAM_SUELO):  # NO cabe muy bien
     #    for y in range(ANCHO_MURO, SCREEN_HEIGHT - TAM_SUELO, TAM_SUELO):
-    #        suelo = arcade.Sprite("sprites_master\\SueloPequeño.png")
+    #        suelo = arcade.Sprite("sprites_master" + os.path.sep + "SueloPequeño.png")
     #        suelo.left = x
     #        suelo.bottom = y
     #        room.floor_list.append(suelo)
-
 
     # If you want coins or monsters in a level, then add that code here.
     # Load the background image for this level.
@@ -104,22 +106,27 @@ class SteamPunkGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Juego")  # Poner nombre del juego
         # Sprite lists
         self.player_list = None
+        self.bullet_list = None
         # Habitacion
         self.current_room = 0
         self.rooms = None
         # Set up the player
         self.jugador = None
         self.physics_engine = None
+        # Variables para el disparo del jugador
+        self.velocidad_disparo = 10
 
     def setup(self):
         arcade.set_background_color(arcade.color.WOOD_BROWN)
         # Sprite lists
         self.player_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
         # Create the player
         self.jugador = Jugador.Jugador()  # OJO!
         self.jugador.center_x = 100
         self.jugador.center_y = 100
         self.player_list.append(self.jugador)
+
         # Rooms
         self.rooms = []  # lista de todas las habitaciones
         room = setup_room_1()  # convendria hacer un for cuando se pongan mas
@@ -136,11 +143,14 @@ class SteamPunkGame(arcade.Window):
         self.rooms[self.current_room].wall_list.draw()
         # self.rooms[self.current_room].floor_list.draw()
         self.player_list.draw()
+        self.bullet_list.draw()
 
     def on_update(self, delta_time: float = 1 / 60):
         # Actualizar todos los sprites
         self.physics_engine.update()
         self.jugador.update_animation()
+        self.bullet_list.update()
+
         # Mirar en que habitación estamos y si necesitamos cambiar a otra
         if self.jugador.center_x > SCREEN_WIDTH and self.current_room == 0:
             self.current_room = 1
@@ -153,8 +163,19 @@ class SteamPunkGame(arcade.Window):
                                                              self.rooms[self.current_room].wall_list)
             self.jugador.center_x = SCREEN_WIDTH
 
+        # Actualizar balas jugador
+        # Loop through each bullet
+        for bala in self.bullet_list:
+            # Mirar si ha chocado con una pared
+            hit_list = arcade.check_for_collision_with_list(bala, self.rooms[self.current_room].wall_list)
+            # Si choca contra una pared, eliminar la bala
+            if len(hit_list) > 0:
+                bala.remove_from_sprite_lists()
+
+
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
+        # Movimiento
         if key == arcade.key.UP or key == arcade.key.W:
             self.jugador.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN or key == arcade.key.S:
@@ -163,6 +184,11 @@ class SteamPunkGame(arcade.Window):
             self.jugador.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.jugador.change_x = MOVEMENT_SPEED
+
+        # Disparo
+        if key == arcade.key.Q:
+            bala = self.jugador.disparar(self.jugador, self.velocidad_disparo)
+            self.bullet_list.append(bala)
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
