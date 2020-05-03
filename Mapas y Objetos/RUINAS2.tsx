84ef441e323e183e121cb1dd0a6426637f34cd4b
Tomás García <tomas.garcias@edu.uah.es>
2020-05-03 18:47:18 +0200

    Update RUINAS2.tsx

--- Mapas y Objetos/RUINAS2.tsx
+++ Mapas y Objetos/RUINAS2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="RUINAS2" tilewidth="900" tileheight="900" tilecount="1" columns="1">
- <image source="../GitHub/proyectovideojuego-equipo-c/sprites_master/RUINAS2.png" width="900" height="900"/>
+ <image source="../sprites_master/RUINAS2.png" width="900" height="900"/>
 </tileset>
