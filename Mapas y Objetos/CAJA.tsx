bcb54de9de196075b41d10197e1965508fc29bf0
Tomás García <tomas.garcias@edu.uah.es>
2020-05-03 18:46:43 +0200

    Update CAJA.tsx

--- Mapas y Objetos/CAJA.tsx
+++ Mapas y Objetos/CAJA.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="CAJA" tilewidth="10" tileheight="10" tilecount="25" columns="5">
  <grid orientation="orthogonal" width="9" height="9"/>
- <image source="sprites_master/CAJA.png" width="50" height="50"/>
+ <image source="../sprites_master/CAJA.png" width="50" height="50"/>
 </tileset>
