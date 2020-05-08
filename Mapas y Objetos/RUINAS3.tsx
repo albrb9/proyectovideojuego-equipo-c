03d10470eda79c571e51bb2cf638d451aced170b
Tomás García <tomas.garcias@edu.uah.es>
2020-05-08 16:47:34 +0200

    Update RUINAS3.tsx

--- Mapas y Objetos/RUINAS3.tsx
+++ Mapas y Objetos/RUINAS3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="RUINAS3" tilewidth="9" tileheight="9" tilecount="6561" columns="81">
- <image source="../sprites_master/RUINAS3.png" width="734" height="735"/>
+<tileset version="1.2" tiledversion="1.3.3" name="RUINAS3" tilewidth="9" tileheight="9" tilecount="10000" columns="100">
+ <image source="../sprites_master/RUINAS3.png" width="900" height="900"/>
 </tileset>
