5900ddfab338d2ceb7fc2e5f2b084296f5e3b5df
Tomás García <tomas.garcias@edu.uah.es>
2020-05-25 22:20:28 +0200

    Create MAQUINA.tsx

---
+++ Mapas y Objetos/MAQUINA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="MAQUINA" tilewidth="10" tileheight="10" tilecount="50" columns="5">
+ <image source="../sprites_master/MAQUINA.png" width="50" height="100"/>
+</tileset>
