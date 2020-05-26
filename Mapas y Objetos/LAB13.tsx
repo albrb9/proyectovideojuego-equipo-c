d252b0cca539e38a94190dda797e134dea405420
Tomás García <tomas.garcias@edu.uah.es>
2020-05-26 18:21:45 +0200

    Create LAB13.tsx

---
+++ Mapas y Objetos/LAB13.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="LAB13" tilewidth="9" tileheight="9" tilecount="10000" columns="100">
+ <image source="../sprites_master/LAB13.png" width="900" height="900"/>
+</tileset>
